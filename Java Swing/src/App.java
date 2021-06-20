1800fdb9c23451f886c44fdace03149439be8cd4
Josip-github <josipcota1@gmail.com>
2021-06-20 07:10:05 +0200

    SwingUtilities.invokeLater
    Neki smatraju da ovaj korak nije potreban, neki misle da je, zbog
    mogućeg kasnijeg multithreadinga pa zbog toga da se ovaj program izvrši
    u posebnom threadu

--- Java Swing/src/App.java
+++ Java Swing/src/App.java
@@ -1,14 +1,22 @@
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class App {
 
 	public static void main(String[] args) {
 
-		JFrame frame = new JFrame("Hello world");
+		SwingUtilities.invokeLater(new Runnable(){
+
+			public void run() {
+				JFrame frame = new JFrame("Hello world");
+				frame.setSize(600, 500);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+			
+		});
 		
-		frame.setSize(600, 500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		
 	}
 
 }
