f50994d60f7304c788719ea6bf207d72c46b47a4
Josip-github <josipcota1@gmail.com>
2021-06-21 15:13:09 +0200

    Razvijanje klase FormPanel

--- Java Swing/src/FormPanel.java
+++ Java Swing/src/FormPanel.java
@@ -1,12 +1,30 @@
 import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 public class FormPanel extends JPanel {
 
+	private JLabel nameLabel;
+	private JLabel ocupationLabel;
+	private JTextField nameField;
+	private JTextField occupationField;
+	private JButton okBtn;
+
+	
 	public FormPanel() {
+		
+		nameLabel = new JLabel("Name: ");
+		ocupationLabel = new JLabel("Occupation: ");
+		nameField = new JTextField(10);
+		occupationField = new JTextField(10);
+		
+		okBtn = new JButton("OK");
+		
 		Dimension dim = getPreferredSize();
 		dim.width = 250;
 		setPreferredSize(dim);
