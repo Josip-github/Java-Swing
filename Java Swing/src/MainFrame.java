e30eba3ae60bcdd00d482d92800daf096d363830
Josip-github <josipcota1@gmail.com>
2021-06-21 07:43:12 +0200

    Uvođenje varijable FormPanel
    Smještanje na lijevu stranu preko add, BorderLayout.west

--- Java Swing/src/MainFrame.java
+++ Java Swing/src/MainFrame.java
@@ -11,6 +11,7 @@ public class MainFrame extends JFrame {
 	private TextPanel textPanel;
 	private JButton btn;
 	private Toolbar toolbar;
+	private FormPanel formPanel;
 	
 	public MainFrame() {
 		super("Hello World");
@@ -20,6 +21,7 @@ public class MainFrame extends JFrame {
 		textPanel = new TextPanel();
 		btn = new JButton("Click me!");
 		toolbar = new Toolbar();
+		formPanel = new FormPanel();
 		
 		toolbar.setStringListener(new StringListener() {
 			
@@ -37,6 +39,7 @@ public class MainFrame extends JFrame {
 			}
 		});*/
 		
+		add(formPanel, BorderLayout.WEST);
 		add(toolbar, BorderLayout.NORTH);
 		add(textPanel, BorderLayout.CENTER);
 		
